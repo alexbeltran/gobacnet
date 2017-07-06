1bbe587fd8e78b720eee9c4c3cf8228cfd60b8e9
Alex <alex.e.beltran@gmail.com>
2017-07-05 20:44:08 -0700

    Accidentally reinvented the ParseCider commander found in the go net package.

--- device.go
+++ device.go
@@ -34,8 +34,6 @@ package gobacnet
 import (
 	"fmt"
 	"net"
-	"strconv"
-	"strings"
 
 	"github.com/alexbeltran/gobacnet/tsm"
 )
@@ -52,38 +50,11 @@ type Client struct {
 
 // getBroadcast uses the given address with subnet to return the broadcast address
 func getBroadcast(addr string) (string, error) {
-	split := strings.Split(addr, "/")
-	if len(split) != 2 {
-		return "", fmt.Errorf("%s is not a valid address. Are you missing a subnet?", addr)
-	}
-	addr = split[0]
-	subnet, err := strconv.Atoi(split[1])
+	_, ipnet, err := net.ParseCIDR(addr)
 	if err != nil {
 		return "", err
 	}
-
-	// First we are going to convert the string address to 32 bit address
-	parts := strings.Split(addr, ".")
-	var b uint32
-	b = 0
-	for i, p := range parts {
-		d, err := strconv.Atoi(p)
-		if err != nil {
-			return "", err
-		}
-
-		b = b + (uint32(d)&0xFF)<<(8*uint(3-i))
-	}
-
-	// Now we can apply the mask.
-	mask := uint32(0xFFFFFFFF >> uint(subnet))
-	b |= mask
-
-	ip := make([]uint8, 4)
-	for i := 0; i < 4; i++ {
-		ip[i] = uint8(b >> uint8(24-8*i))
-	}
-	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]), nil
+	return ipnet.String(), nil
 }
 
 func NewClient(inter string) (*Client, error) {
