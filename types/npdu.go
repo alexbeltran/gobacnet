940a71ce38a5d57562821e61919711016b97bf14
Alex <alex.e.beltran@gmail.com>
2017-07-07 23:11:30 -0700

    Change metadata information to info. This will make encoding/decoding a bit easier.

--- types/npdu.go
+++ types/npdu.go
@@ -1,9 +1,20 @@
 package types
 
+// NPDUMetadata includes additional metadata about npdu message
+type NPDUMetadata byte
+type NPDUPriority byte
+
+const (
+	LifeSafety        NPDUPriority = 3
+	CriticalEquipment NPDUPriority = 2
+	Urgent            NPDUPriority = 1
+	Normal            NPDUPriority = 0
+)
+
 type NPDU struct {
 	Version uint8
 	// Info stores raw NPDU metadata
-	Info byte
+	Info NPDUMetadata
 
 	// Destination (optional)
 	Destination *Address
@@ -14,70 +25,61 @@ type NPDU struct {
 	VendorId uint16
 }
 
-type NPDUPriority byte
-
-const (
-	LifeSafety        NPDUPriority = 3
-	CriticalEquipment NPDUPriority = 2
-	Urgent            NPDUPriority = 1
-	Normal            NPDUPriority = 0
-)
-
 const maskNetworkLayerMessage = 1 << 7
 const maskDestination = 1 << 5
 const maskSource = 1 << 3
 
 // General setter for the info bits using the mask
-func (n *NPDU) setInfoMask(b bool, mask byte) {
+func (meta *NPDUMetadata) setInfoMask(b bool, mask byte) {
 	if b {
-		n.Info = n.Info | mask
+		*meta = *meta | NPDUMetadata(mask)
 	} else {
 		var m byte = 0xFF
 		m = m - mask
-		n.Info = n.Info & m
+		*meta = *meta & NPDUMetadata(m)
 	}
 }
 
 // CheckMask uses mask to check bit position
-func (n *NPDU) checkMask(mask byte) bool {
-	return (n.Info & mask) > 0
+func (meta *NPDUMetadata) checkMask(mask byte) bool {
+	return (*meta & NPDUMetadata(mask)) > 0
 
 }
 
 // IsNetworkLayerMessage returns true if it is a network layer message
-func (n *NPDU) IsNetworkLayerMessage() bool {
+func (n *NPDUMetadata) IsNetworkLayerMessage() bool {
 	return n.checkMask(maskNetworkLayerMessage)
 }
 
-func (n *NPDU) SetNetworkLayerMessage(b bool) {
+func (n *NPDUMetadata) SetNetworkLayerMessage(b bool) {
 	n.setInfoMask(b, maskNetworkLayerMessage)
 }
 
 // Priority returns priority
-func (n *NPDU) Priority() NPDUPriority {
+func (n *NPDUMetadata) Priority() NPDUPriority {
 	// Encoded in bit 0 and 1
-	return NPDUPriority(n.Info & 3)
+	return NPDUPriority(byte(*n) & 3)
 }
 
 // SetPriority for NPDU
-func (n *NPDU) SetPriority(p NPDUPriority) {
+func (n *NPDUMetadata) SetPriority(p NPDUPriority) {
 	// Clear the first two bits
-	n.Info &= (0xF - 3)
-	n.Info |= byte(p)
+	*n &= (0xF - 3)
+	*n |= NPDUMetadata(p)
 }
 
-func (n *NPDU) HasDestination() bool {
+func (n *NPDUMetadata) HasDestination() bool {
 	return n.checkMask(maskDestination)
 }
 
-func (n *NPDU) SetDestination(b bool) {
+func (n *NPDUMetadata) SetDestination(b bool) {
 	n.setInfoMask(b, maskDestination)
 }
 
-func (n *NPDU) HasSource() bool {
+func (n *NPDUMetadata) HasSource() bool {
 	return n.checkMask(maskSource)
 }
 
-func (n *NPDU) SetSource(b bool) {
+func (n *NPDUMetadata) SetSource(b bool) {
 	n.setInfoMask(b, maskSource)
 }
