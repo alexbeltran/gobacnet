0f1e03d8e68de32c0da67670d77ff4caa2ba38e3
Alex <alex.e.beltran@gmail.com>
2017-07-14 01:21:52 -0700

    Add additional decoding test based on known values sampled from the network

---
+++ encoding/service_test.go
@@ -0,0 +1,90 @@
+package encoding
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"log"
+	"testing"
+
+	bactype "github.com/alexbeltran/gobacnet/types"
+)
+
+func TestReadPropertyService(t *testing.T) {
+	// This value is based on a known sample
+	expected := []byte{129, 10, 0, 22, 1, 36, 9, 124, 1, 29, 255, 0, 5, 1, 12,
+		12, 0, 0, 0, 1, 25, 85}
+
+	e := NewEncoder()
+	//s := `{"ID":24289,"MaxAPDU":480,"Address":{"Mac":"ChQAzLrA","MacLen":6,"Net":2428,"Adr":"HQ==","AdrLen":1}}`
+	var mac []uint8
+	var adr []uint8
+	json.Unmarshal([]byte("\"ChQAzLrA\""), &mac)
+	json.Unmarshal([]byte("\"HQ==\""), &adr)
+	readProp := bactype.ReadPropertyData{
+		ObjectType:     0,
+		ObjectInstance: 1,
+		ObjectProperty: 85,
+		ArrayIndex:     0xFFFFFFFF,
+	}
+
+	dest := bactype.Address{
+		Net:    2428,
+		Mac:    mac,
+		MacLen: 6,
+		Len:    1,
+		Adr:    adr,
+	}
+	e.NPDU(bactype.NPDU{
+		Version:               bactype.ProtocolVersion,
+		IsNetworkLayerMessage: false,
+		ExpectingReply:        true,
+		HopCount:              bactype.DefaultHopCount,
+		Priority:              bactype.Normal,
+		Destination:           &dest,
+	})
+	e.ReadProperty(1, readProp)
+	data := e.Bytes()
+
+	enc := NewEncoder()
+	bv := bactype.BVLC{
+		Type:     bactype.BVLCTypeBacnetIP,
+		Function: bactype.BacFuncUnicast,
+		Length:   4 + uint16(len(data)),
+		Data:     data,
+	}
+	enc.BVLC(bv)
+
+	raw := enc.Bytes()
+	for i, b := range raw {
+		if expected[i] != b {
+			t.Errorf("Error during decoding: %x does not equal expected %x", b, expected[i])
+		}
+	}
+	if len(raw) != len(expected) {
+		t.Fatalf("There is a mismatch in sizes. Got: %d, Expected:%d", len(raw), len(expected))
+	}
+	t.Logf("Length: %d", len(raw))
+}
+
+func TestReadPropertyResponse(t *testing.T) {
+	// This value is based on a known sample
+	in := []byte{48, 1, 12, 12, 0, 0, 0, 1, 25, 85, 62, 68, 192, 160, 0, 0, 63}
+	d := NewDecoder(in)
+	apdu := bactype.APDU{}
+	d.APDU(&apdu)
+
+	rpd := bactype.ReadPropertyData{}
+	err := d.ReadProperty(&rpd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	apd := NewDecoder(rpd.ApplicationData)
+	tag, _, value := apd.tagNumberAndValue()
+	log.Printf("Tag:%d, Value:%d", tag, value)
+	var x float32
+	if tag == 4 {
+		binary.Read(apd.buff, binary.BigEndian, &x)
+		log.Printf("Real: %f", x)
+	}
+
+}
