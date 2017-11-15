e00b20edf79609416a22b59499b56a1e32bb27ae
Alex Beltran <alex.e.beltran@gmail.com>
2017-11-15 14:48:17 -0800

    Move printing properties to property package. Made read multi a child of readprop

--- baccli/cmd/readmultiprop.go
+++ baccli/cmd/readmultiprop.go
@@ -15,14 +15,13 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/alexbeltran/gobacnet/property"
 	"github.com/spf13/cobra"
 )
 
 // readmultipropCmd represents the readmultiprop command
 var readmultipropCmd = &cobra.Command{
-	Use:   "readmultiprop",
+	Use:   "multi",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,12 +30,15 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("readmultiprop called")
+		if listProperties {
+			property.PrintAll()
+			return
+		}
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(readmultipropCmd)
+	readpropCmd.AddCommand(readmultipropCmd)
 
 	// Here you will define your flags and configuration settings.
 
